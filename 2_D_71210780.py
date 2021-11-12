465a75b8d010083c60db5559d17098d818900e82
yosriko <yosriku.rahmat@ti.ukdw.ac.id>
2021-11-12 11:11:47 +0700

    Rata rata uang saku anak

---
+++ 2_D_71210780.py
@@ -0,0 +1,12 @@
+#rata-rata uang saku anaknya Dini
+
+#input
+a1= int(input("Uang saku anak pertama: "))
+a2= int(input("Uang saku anak kedua: "))
+a3= int(input("Uang saku anak ketiga: "))
+
+#proses
+x=(a1+a2+a3)/3
+
+#output
+print("Rata-rata dari", a1, '+', a2, '+',a3, "adalah", x)
