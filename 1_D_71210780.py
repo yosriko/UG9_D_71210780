e9ce3ce1c946346dfe86d5db23df20e6efa640d8
yosriko <yosriku.rahmat@ti.ukdw.ac.id>
2021-11-12 11:10:46 +0700

    Luas layang layang

---
+++ 1_D_71210780.py
@@ -0,0 +1,13 @@
+#menghitung luas layangan
+
+#input
+diagonal1= int(input("Masukkan d1: "))
+diagonal2= int(input("Masukkan d2: "))
+
+#proses
+a= diagonal1
+b= diagonal2
+c= 0.5
+
+#output
+print("Luas layangan: ", c*(a*b),"cm")
