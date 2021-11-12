29aebfa32e957932cad7b489f47c3449f746f0b3
yosriko <yosriku.rahmat@ti.ukdw.ac.id>
2021-11-12 11:12:28 +0700

    Pencatat penambahan belanja

---
+++ 3_D_71210780.py
@@ -0,0 +1,15 @@
+#pencatat penambahan belajaannya
+
+#input
+b1= input("Masukkan daftar belanja 1: ")
+b2= input("Masukkan daftar belanja 2: ")
+t1= input("Tambahan data ke daftar belanja 1: ")
+t2= input("Tambahan data ke daftar belanja 2: ")
+
+#proses
+db1= b1,t1
+db2= b2,t2
+
+#output
+print("Daftar belanja 1 adalah ", b1, ",", t1)
+print("Daftar belanja 2 adalah ", b2, ",", t2)
